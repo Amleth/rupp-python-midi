0917eef815cae96f7caf7251aae85e571006ad1c
Thomas ⬡ Bottini <thomas.bottini@gmail.com>
2024-12-09 09:56:48 +0100

    🌴

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ mid.tracks.append(track)
 
 with open('data.csv', mode='r') as file:
     csv_reader = csv.reader(file)
+
     for row in csv_reader:
         pitch = int(row[0])
         duration = int(row[1])
+
         track.append(Message('note_on', note=pitch, velocity=64, time=0))
         track.append(Message('note_off', note=pitch, velocity=64, time=duration * 480))
 
