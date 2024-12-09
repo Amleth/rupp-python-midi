a2229a5cc9f037f9149aa9cede5ee6ea011f2be4
Thomas ⬡ Bottini <thomas.bottini@gmail.com>
2024-12-09 10:42:23 +0100

    🌴

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from mido import bpm2tempo, Message, MidiFile, MidiTrack, second2tick
 bpm = 60
 
 mid = MidiFile()
-track = MidiTrack()
-mid.tracks.append(track)
+track_left = MidiTrack()
+track_right = MidiTrack()
+mid.tracks.append(track_left)
+mid.tracks.append(track_right)
 
 
 def convert_note_name_to_midi_value(x):
@@ -23,15 +25,20 @@ def convert_note_name_to_midi_value(x):
     return notes[note_name] + 12 * int(x[1:])
 
 
-with open('data.csv', mode='r') as file:
-    csv_reader = csv.reader(file)
+def process_track(f, t):
+    with open(f, mode='r') as file:
+        csv_reader = csv.reader(file)
 
-    for row in csv_reader:
-        pitch = convert_note_name_to_midi_value(row[0])
-        duration = float(row[1])
-        duration = second2tick(duration, mid.ticks_per_beat, bpm2tempo(bpm))
+        for row in csv_reader:
+            pitch = convert_note_name_to_midi_value(row[0])
+            duration = float(row[1])
+            duration = second2tick(duration, mid.ticks_per_beat, bpm2tempo(bpm))
 
-        track.append(Message('note_on', note=pitch, velocity=64, time=0))
-        track.append(Message('note_off', note=pitch, velocity=64, time=duration))
+            t.append(Message('note_on', note=pitch, velocity=64, time=0))
+            t.append(Message('note_off', note=pitch, velocity=64, time=duration))
+
+
+process_track('left.csv', track_left)
+process_track('right.csv', track_right)
 
 mid.save('file.mid')
